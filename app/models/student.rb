6f20b951e0af209012b432fe3b1737b204efa55b
James Clement <jameseclement@gmail.com>
2019-02-08 15:30:42 -0500

    add student full-name method

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,8 @@
 class Student < ApplicationRecord
   has_many :reviews, dependent: :destroy
   has_many :klasses, through: :reviews, dependent: :destroy
+
+  def full_name
+    "#{self.first_name} + #{self.last_name}"
+  end
 end
