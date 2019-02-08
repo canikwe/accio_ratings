d354f8ccb6436f33aae81e2f1f60f8aaf0a21eee
canikwe <c.anikwe@gmail.com>
2019-02-08 16:11:28 -0500

    create student show page

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,6 +1,15 @@
 class StudentsController < ApplicationController
+
+  before_action :get_student, only: [:show, :destroy, :edit, :update]
   def show
   end
 
+  private
+  def get_student
+    @student = Student.find(params[:id])
+  end
 
+  def student_params
+    params.require(:student).permit(:first_name, :last_name, :username, :grad_year)
+  end
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -3,6 +3,6 @@ class Student < ApplicationRecord
   has_many :klasses, through: :reviews, dependent: :destroy
 
   def full_name
-    "#{self.first_name} + #{self.last_name}"
+    "#{self.first_name} #{self.last_name}"
   end
 end
