a752c991899989a9ac73b0c28cab59b91f674307
canikwe <c.anikwe@gmail.com>
2019-02-11 10:18:13 -0500

    create average rating method and build instructor showpage

--- app/models/klass.rb
+++ app/models/klass.rb
@@ -2,4 +2,10 @@ class Klass < ApplicationRecord
   belongs_to :subject
   belongs_to :instructor
   has_many :reviews, dependent: :destroy
+
+
+  def average_overall
+    overall = self.reviews.map {|r| r.overall}
+    overall.inject(:+).to_f / overall.count
+  end
 end
