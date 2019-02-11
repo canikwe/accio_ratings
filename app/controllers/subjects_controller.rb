af29275f6a73fcedc08b5000bcd9ea47bcbadbbb
canikwe <c.anikwe@gmail.com>
2019-02-11 17:29:41 -0500

    create new subject view

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -8,4 +8,22 @@ class SubjectsController < ApplicationController
     @subjects = Subject.all
   end
 
+  def new
+    @subject = Subject.new
+  end
+
+  def create
+    @subject = Subject.new(subject_params)
+    if @subject.save
+      redirect_to new_klasses_path
+    else render :new
+    end
+  end
+
+
+  private
+
+  def subject_params
+    params.require(:subject).permit(:name)
+  end
 end
