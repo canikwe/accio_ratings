801c8477827502d197010024431f0153879d03e6
canikwe <c.anikwe@gmail.com>
2019-02-11 21:41:12 -0500

    reviews validations

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,48 +1,49 @@
 class ReviewsController < ApplicationController
 
-before_action :get_reviews, only: [:show, :edit, :update, :destroy]
+  before_action :get_reviews, only: [:show, :edit, :update, :destroy]
 
-def new
-  @review = Review.new
-end
+  def new
+    @review = Review.new
+  end
 
-def create
-  @review = Review.create(review_params)
-  redirect_to review_path(@review)
+  def create
+    @review = Review.new(review_params)
+    if @review.save
+      redirect_to review_path(@review)
+    else render :new
+    end
+  end
 
-end
+  def show
+  end
 
-def show
-end
+  def edit
+  end
 
-def edit
-end
+  def update
+    if @review.update(review_params)
+      redirect_to review_path(@review)
+    else
+      flash.notice = "Username and password do not match"
+      render :edit
+    end
+  end
 
-def update
-  @review.update(review_params)
-  redirect_to review_path(@review)
-end
+  def destroy
+    k = @review.klass
+    @review.destroy
+    redirect_to klass_path(k)
+  end
 
-def destroy
-  k = @review.klass
-  @review.destroy
-  redirect_to klass_path(k)
-end
+  private
 
+  def get_reviews
+    @review = Review.find(params[:id])
+  end
 
-
-
-
-private
-
-def get_reviews
-  @review = Review.find(params[:id])
-end
-
-def review_params
-  params.require(:review).permit(:title, :content, :overall, :difficulty, :intrigue, :recommendation, :klass_id, :student_id)
-  ## Will need to check on :student param-- how can you choose who is writing the review?
-end
-
+  def review_params
+    params.require(:review).permit(:title, :content, :overall, :difficulty, :intrigue, :recommendation, :klass_id, :student_id)
+    ## Will need to check on :student param-- how can you choose who is writing the review?
+  end
 
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,12 @@
 class Review < ApplicationRecord
+  # Associations
   belongs_to :student
   belongs_to :klass
+
+  # Validations
+  validates :title, :overall, :difficulty, :intrigue, :recommendation, presence: true
+  validates :content, presence: true, length: {minimum: 25, maximum: 300, message: "Reviews must be between 25 and 300 characters"}
+  validates :student_id, uniqueness: {scope: :klass_id, message: "You have already reviewed this class"}
+
+
 end
