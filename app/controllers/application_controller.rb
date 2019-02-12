eb01528e223ab77699fb90a12f1d0fc93e76deaa
James Clement <jameseclement@gmail.com>
2019-02-12 09:48:20 -0500

    fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+
+# before_action :rootmaker
+#
+# def rootmaker
+#
+#   if session[:student_id].nil?
+#     render 'sessions/new'
+#   end
+# end
+
+
+
 end
